9672f8231993d84dca42daf113c719e9e37d3f3f
olegesan <37033572+olegesan@users.noreply.github.com>
2020-02-20 15:26:40 -0500

    fixed not working menu navigation keys

--- BeyondJoust/Assets/Scripts/SelectOnInput.cs
+++ BeyondJoust/Assets/Scripts/SelectOnInput.cs
@@ -6,26 +6,26 @@ public class SelectOnInput : MonoBehaviour
 {
 	public EventSystem eventSystem;
 	public GameObject selectedObject;
-	private bool buttonSelected = false;
+	private bool buttonSelected;
     // Start is called before the first frame update
     void Start()
     {
-        
     }
 
     // Update is called once per frame
     void Update()
     {
-    	Debug.Log(buttonSelected);
-    	// if()
+    	
         if(Input.GetAxisRaw("Vertical") != 0 && buttonSelected == false){
-        	Debug.Log("Reached vertical change");
         	eventSystem.SetSelectedGameObject(selectedObject);
         	buttonSelected = true;
         }
 
+
+
     }
-    private void onDisable(){
+
+    void OnDisable(){
     	buttonSelected = false;
     }
 }
